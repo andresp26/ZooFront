d7d93be7879db1aaf4738a116a888367f1aee520
Andres <andresp-26@hotmail.com>
2019-05-14 22:08:28 -0500

    Creacion paginas Shows, Animales, Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { AnimalesComponent } from './Pages/animales/animales.component';
+import { ShowsComponent } from './Pages/shows/shows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AnimalesComponent,
+    ShowsComponent
   ],
   imports: [
     BrowserModule,
