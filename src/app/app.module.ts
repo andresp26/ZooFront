9e7b09cef3a70b1fee1542379e3c1e33a610f2ec
unknown <camgaro.04@gmail.com>
2019-05-16 00:48:04 -0500

    se agrega pantalla inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { AnimalesComponent } from './Pages/animales/animales.component';
 import { ShowsComponent } from './Pages/shows/shows.component';
+import { NavBarComponent } from './Pages/shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AnimalesComponent,
-    ShowsComponent
+    ShowsComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
